7f6f537308b1e634a66e5f05fd62217a59f735be
brineamelia <brineamelia@gmail.com>
2022-10-13 14:11:37 +0000

    Setup first user request and show format of data input required

--- run.py
+++ run.py
@@ -12,8 +12,16 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('sales_data')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data of units from the last selling day.")
+    print("Data should be 5 numbers, separated by commas")
+    print("Example: 1,2,3,4,5\n")
 
-data = sales.get_all_values()
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
 
-print(data)
+
+get_sales_data()
